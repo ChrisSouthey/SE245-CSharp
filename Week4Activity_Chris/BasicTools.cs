bb27578be001921bf3b651ab0cc7be7769d241b2
008022228 <008022228@N203D5-23>
2024-04-19 13:02:39 -0400

    Week 4 activity

---
+++ Week4Activity_Chris/BasicTools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Activity_Chris
+{
+    class BasicTools
+    {
+        public static void Pause()
+        {
+            Console.WriteLine("\nPress Any Key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
--- Week4Activity_Chris/Program.cs
+++ Week4Activity_Chris/Program.cs
@@ -161,9 +161,69 @@ namespace Week4Activity_Chris
 
             do
             {
-                Console.Write("\nPlease ")
-            }
+                Console.Write("\nPlease enter the Date published: ");
+                DateTime dtTempDate;
+                blnResult = DateTime.TryParse(Console.ReadLine(), out dtTempDate);
+
+                if (blnResult == false)
+                {
+                    Console.Write("\nIncorrect Date format. Please try again. (Ex. 03/13/2005) ");
+                }
+                else
+                {
+                    temp.DatePublished = dtTempDate;
+                }
+            } while (blnResult == false);
+
+
+            do
+            {
+                Console.Write("\nPlease enter the # of pages: ");
+                int intTempPages;
+                blnResult = Int32.TryParse(Console.ReadLine(), out intTempPages);
+
+                if (blnResult == false)
+                {
+                    Console.Write("\nIncorrect # of pages. Please try again. (Ex. 115) ");
+                }
+                else
+                {
+                    temp.Pages = intTempPages;
+                }
+            } while (blnResult == false);
+
+
+            do
+            {
+                Console.Write("\nPlease enter the Price of the book: $");
+                double dblTempPrice;
+                blnResult = Double.TryParse(Console.ReadLine(), out dblTempPrice);
+
+                if (blnResult == false)
+                {
+                    Console.Write("\nIncorrect price. Please try again. (Ex. 14.95) ");
+                }
+                else
+                {
+                    temp.Price = dblTempPrice;
+                }
+            } while (blnResult == false);
+
+
+
+            Console.Write("\n\nYou have entered the following book:");
+            Console.Write($"\nTitle: {temp.Title}");
+            Console.Write($"\nWritten by: {temp.AuthFirst} {temp.AuthLast}");
+            Console.Write($"\nEmail: {temp.Email}");
+            Console.Write($"\nPublished: {temp.DatePublished.ToShortDateString()}");
+            Console.Write($"\nPages: {temp.Pages}");
+            Console.Write($"\nPrice: {temp.Price}");
+
+            BasicTools.Pause();
+
         }
+
+
 
         
         
